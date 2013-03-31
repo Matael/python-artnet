e023ea369cb811756c15355bb7f38147821ed268
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-03-31 20:14:14 +0200

    Add condition to execute main if script is call as is.

--- src/artnet/listener.py
+++ src/artnet/listener.py
@@ -4,17 +4,6 @@ import socket
 import time
 
 
-def main():
-	if(len(sys.argv) < 2):
-		print "\nUsage:\n\tartnet_listener [address]\n"
-		sys.exit(1)
-	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(('', 6454))
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	l = ArtPollListener(sock, sys.argv[1])
-	l.run()
-	
 def getsock():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.bind(('', 6454))
@@ -63,3 +52,19 @@ class ArtPollListener(threading.Thread):
 				r = packet.PollReplyPacket([], source=(ip_address, 6454))
 				l = self.sock.sendto(r.encode(), (p.source[0], packet.ARTNET_PORT))
 				print 'poll reply packet: %s' % r
+
+
+def main():
+	if(len(sys.argv) < 2):
+		print "\nUsage:\n\tartnet_listener [address]\n"
+		sys.exit(1)
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(('', 6454))
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	l = ArtPollListener(sock, sys.argv[1])
+	l.run()
+
+if __name__=='__main__':
+    main()
+
