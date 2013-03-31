7e08ffc6f486f33a8436ce89d63bd37599b992fc
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-03-31 19:26:41 +0200

    Fix whitespaces and PEP8 compliance

--- src/artnet/rig.py
+++ src/artnet/rig.py
@@ -7,7 +7,9 @@ import pkg_resources as pkg
 
 from artnet import dmx
 
+
 log = logging.getLogger(__name__)
+
 
 _default_rig = None
 def get_default_rig():
@@ -16,20 +18,25 @@ def get_default_rig():
 		_default_rig = load(os.path.expanduser("~/.artnet-rig.yaml"))
 	return _default_rig
 
+
 def load(config_path):
+
 	rig = Rig()
+
 	with open(config_path, 'r') as f:
 		rig_data = yaml.safe_load(f)
-	
+
 	rig.name = rig_data['name']
 	for name, f in rig_data['fixtures'].items():
 		rig.fixtures[name] = fixtures.Fixture.create(f['address'], f['config'])
-	
+
 	for name, groups in rig_data['groups'].items():
 		rig.groups[name] = fixtures.FixtureGroup([
 			rig.fixtures[g] for g in groups
 		])
+
 	return rig
+
 
 class Rig(object):
 	def __init__(self, name=None):
