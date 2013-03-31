80fc0d2bdb2c49d09bce6120c98b5deeb194842e
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-03-31 19:36:36 +0200

    Documentation

--- src/artnet/rig.py
+++ src/artnet/rig.py
@@ -20,6 +20,12 @@ def get_default_rig():
 
 
 def load(config_path):
+    """ Rigfile oading function
+
+    Take path to a rigfile as argument and return a Rig object (pythonic
+    and object-oriented version of the given rigfile)
+
+    """
 
 	rig = Rig()
 
@@ -39,6 +45,42 @@ def load(config_path):
 
 
 class Rig(object):
+    """
+    Class handling rigfiles.
+
+    These files are YAML describing available elements to control.
+
+    Example of rigfile :
+
+    {
+        "name": "Example Rig",
+        "fixtures": {
+            "slimpar_1": {
+                "address": 1,
+                "config": "chauvet/slimpar-64.yaml"
+            },
+            "slimpar_2": {
+                "address": 8,
+                "config": "chauvet/slimpar-64.yaml"
+            },
+            "slimpar_3": {
+                "address": 15,
+                "config": "chauvet/slimpar-64.yaml"
+            },
+            "slimpar_4": {
+                "address": 22,
+                "config": "chauvet/slimpar-64.yaml"
+            },
+        },
+        "groups": {
+            "all": ["slimpar_1", "slimpar_2", "slimpar_3", "slimpar_4"],
+            "odds": ["slimpar_1", "slimpar_3"],
+            "evens": ["slimpar_2", "slimpar_4"],
+        },
+    }
+
+
+    """
 	def __init__(self, name=None):
 		self.name = repr(self)
 		self.groups = {}
